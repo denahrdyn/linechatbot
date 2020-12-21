cbb1dd6e7d567fe650b88c90d472071842b291e4
Dena Herdiyani <denaherdiyani@gmail.com>
2020-12-21 12:02:52 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use \LINEBot;
+use \LINEBot\HTTPClient\CurlHTTPClient;
+use \LINEBot\MessageBuilder\MultiMessageBuilder;
+use \LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINEBot\MessageBuilder\StickerMessageBuilder;
+use \LINEBot\SignatureValidator as SignatureValidator;
+
+$pass_signature = true;
+
+// set LINE channel_access_token and channel_secret
+$channel_access_token = "GXBAWWuo6f3nnMXpfd0THXW8UUrjb+pK32Gy9bJscPGkDPXJH5HPkuYTnvs7Kum4qOOX4UX8afT3r2QCCVctIvo1UWXivpPS+oPbziVyyPEDyAn9c/8vvHr9hIjdnygjvnYxHVTkn5zv9kVDblQfhwdB04t89/1O/w1cDnyilFU=";
+$channel_secret = "782cac53e7799cd2bb91e7f579e2e51f";
+
+// inisiasi objek bot
+$httpClient = new CurlHTTPClient($channel_access_token);
+$bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+
+$configs = [
+    'settings' => ['displayErrorDetails' => true],
+];
+$app = new Slim\App($configs);
+
+$app->get('/', function($req, $res) {
+    echo 'Hello World!';
+});
+
+// buat route untuk webhook
+$app->post('/webhook', function ($req, $res) use ($bot, $httpClient, $pass_signature) 
+{
+    // get request body and line signature header
+    $body = file_get_contents('php://input');
+    $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+
+    // log body and signature
+    file_put_contents('php://stderr', 'Body: '.$body);
+
+    if($pass_signature === false)
+    {
+        // is LINE_SIGNATURE exists in request header?
+        if (empty($signature))
+        {
+            return $res->withStatus(400, 'Signature not set');
+        }
+
+        // is this request comes from LINE?
+        if (! SignatureValidator::validatorSignature($body, $channel_secret, $signature))
+        {
+            return $res->withStatus(400, 'Invalid signature');
+        }
+    }
+
+});
+$app->run();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,164 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'dicoding/line-bot-dicoding',
+  ),
+  'versions' => 
+  array (
+    'dicoding/line-bot-dicoding' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.2.0',
+      'version' => '7.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0aa74dfb41ae110835923ef10a9d803a22d50e79',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '60d379c243457e073cff02bc323a2a86cb355631',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '1.7.0',
+      'version' => '1.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '53330f47520498c0ae1f61f7e2c90f55690c06a3',
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '6.1.0',
+      'version' => '6.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a3d3385aabed6e53071766587491d6e7fd2021e8',
+    ),
+    'nikic/fast-route' => 
+    array (
+      'pretty_version' => 'v1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '181d480e08d9476e61381e04a71b34dc0432e812',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-factory' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12ac7fcd07e5b077433f5f2bee95b3a771bf61be',
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-server-handler' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aff2f80e33b7f026ec96bb42f63242dc50ffcae7',
+    ),
+    'psr/http-server-middleware' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2296f45510945530b9dceb8bcedb5cb84d40c5f5',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '4.7.1',
+      'version' => '4.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0905e0775f8c1cfb3bbcfabeb6588dcfd8b82d3f',
+    ),
+  ),
+);
