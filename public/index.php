afe9c5f09a1346c39849d546d50bbdf490162523
Dena Herdiyani <denaherdiyani@gmail.com>
2020-12-22 16:18:27 +0700

    initial commit

--- public/index.php
+++ public/index.php
@@ -58,80 +58,66 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
     }
 
     $data = json_decode($body, true);
-    if(is_array($data['events'])) 
+    foreach ($data['events'] as $event)
     {
-        foreach ($data['events'] as $event)
+        $message = strtolower($event['message']['text']);
+        if ($message == 'Halo')
         {
-            if ($event['type'] == 'follow')
-            {
-                if ($event['source']['userId'])
-                {
-                  $onboarding = new TextMessageBuilder('Ketik "Cafe" untuk melakukan pencarian Cafe Shop Terfavoritemu di Bandung.');
-                  $result = $bot->replyMessage($event['replyToken'], $onboarding);
-                  return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-                } 
-            } elseif ($event['type'] == 'message')
-            {
-                if($event['message']['type'] == 'text')
-                {
-                    $message = strtolower($event['message']['text']);
-                    if($message == 'Cafe')
-                    {
-                        $carouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder([
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Jardin Cafe", "Address: Jl. Cimanuk No.1A, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40115","https://i.imgur.com/aqtehou.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/jardinbdg'),
-                            ]),
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("La Costille Cafe", "Address: Jl. Prof. Dr. Sutami No.98, Pasteur, Kec. Sukasari, Kota Bandung, Jawa Barat 40533","https://i.imgur.com/5BBpLyE.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/la_costillabdg'),
-                            ]),
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Mimiti Coffee & Space", "Address: Jl. Bukit Pakar Timur Jl. Pakar Bar. No.7, Dago, Kec. Cimenyan, Bandung, Jawa Barat 40198","https://i.imgur.com/9Twqs2T.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/mimiticoffee'),
-                            ]),
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Picknick Cafe", "Address: Jl. Pasir Kaliki No.176, Pasirkaliki, Cicendo, Bandung City, West Java 40173","https://i.imgur.com/kKYhxSf.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/picknick.kaliki'),
-                            ]),
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Please Please Please Cafe", "Address: Jl. Progo No.37, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40115","https://i.imgur.com/YOwVY3o.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/ppplease.eat'),
-                            ]),
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Sejiwa Coffee", "Address: Jl. Progo No.15, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40115","https://i.imgur.com/YxvhEJF.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/sejiwacoffee'),
-                            ]),
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Sudut Pandang", "Address: Jl. Pagermaneuh, RT.05/RW.07, Pagerwangi, Lembang, Kabupaten Bandung Barat, Jawa Barat 40391","https://i.imgur.com/oW9JyfZ.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/sudutpandang.bdg'),
-                            ]),
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Sydwic Cafe", "Address: Cilaki St No.63, Citarum, Bandung Wetan, Bandung City, West Java 40115","https://i.imgur.com/Rwolq3m.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/sydwic'),
-                            ]),
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("The Potting Shed", "Address: Jl. Panumbang Jaya No.5, Ciumbuleuit, Kec. Cidadap, Kota Bandung, Jawa Barat 40142","https://i.imgur.com/S2Xr3P1.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/thehousetourhotel'),
-                            ]),
-                            new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Tilu Kitchen & Patisserie", "Address: LLRE Martadinata St No.81, Citarum, Riau,Bandung, Bandung City, West Java 40115","https://i.imgur.com/XbhIgFU.jpeg",[
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/tilukitchenandpatisserie'),
-                            ]),
-                        ]);
+            $onboarding = new TextMessageBuilder('Ketik "Cafe" untuk melakukan pencarian Cafe Shop Terfavoritemu di Bandung.');
+            $result = $bot->replyMessage($event['replyToken'], $onboarding);
+            return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+        } 
+    
+        $message = strtolower($event['message']['text']);
+        if($message == 'Cafe')
+        {
+            $carouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder([
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Jardin Cafe", "Address: Jl. Cimanuk No.1A, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40115","https://i.imgur.com/aqtehou.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/jardinbdg'),
+                ]),
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("La Costille Cafe", "Address: Jl. Prof. Dr. Sutami No.98, Pasteur, Kec. Sukasari, Kota Bandung, Jawa Barat 40533","https://i.imgur.com/5BBpLyE.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/la_costillabdg'),
+                ]),
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Mimiti Coffee & Space", "Address: Jl. Bukit Pakar Timur Jl. Pakar Bar. No.7, Dago, Kec. Cimenyan, Bandung, Jawa Barat 40198","https://i.imgur.com/9Twqs2T.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/mimiticoffee'),
+                ]),
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Picknick Cafe", "Address: Jl. Pasir Kaliki No.176, Pasirkaliki, Cicendo, Bandung City, West Java 40173","https://i.imgur.com/kKYhxSf.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/picknick.kaliki'),
+                ]),
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Please Please Please Cafe", "Address: Jl. Progo No.37, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40115","https://i.imgur.com/YOwVY3o.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/ppplease.eat'),
+                ]),
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Sejiwa Coffee", "Address: Jl. Progo No.15, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40115","https://i.imgur.com/YxvhEJF.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/sejiwacoffee'),
+                ]),
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Sudut Pandang", "Address: Jl. Pagermaneuh, RT.05/RW.07, Pagerwangi, Lembang, Kabupaten Bandung Barat, Jawa Barat 40391","https://i.imgur.com/oW9JyfZ.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/sudutpandang.bdg'),
+                ]),
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Sydwic Cafe", "Address: Cilaki St No.63, Citarum, Bandung Wetan, Bandung City, West Java 40115","https://i.imgur.com/Rwolq3m.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/sydwic'),
+                ]),
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("The Potting Shed", "Address: Jl. Panumbang Jaya No.5, Ciumbuleuit, Kec. Cidadap, Kota Bandung, Jawa Barat 40142","https://i.imgur.com/S2Xr3P1.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/thehousetourhotel'),
+                ]),
+                new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumTemplateBuilder("Tilu Kitchen & Patisserie", "Address: LLRE Martadinata St No.81, Citarum, Riau,Bandung, Bandung City, West Java 40115","https://i.imgur.com/XbhIgFU.jpeg",[
+                new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka', 'https://www.instagram.com/tilukitchenandpatisserie'),
+                ]),
+            ]);
 
-                        $templateMessage = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('Cafe Hits Bandung', $carouselTemplateBuilder);
-                        $result = $bot->replyMessage($event['replyToken'], $templateMessage);
-                        return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-                    
-                    } elseif ($message != '')
-                    {
-                        $textMessageBuilder = new TextMessageBuilder('Tidak ada hasil ditemukan!');
-                        $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-                        return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-                    }
-                }
-            }
+            $templateMessage = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('Cafe Hits Bandung', $carouselTemplateBuilder);
+            $result = $bot->replyMessage($event['replyToken'], $templateMessage);
+            return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
+        
+        } 
+        
+        $message = strtolower($event['message']['text']);
+        if ($message != '')
+        {
+            $textMessageBuilder = new TextMessageBuilder('Tidak ada hasil ditemukan!');
+            $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+            return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
         }
     }
 });
 
-$app->get('/profile/{userId}', function($request, $response) use ($bot)
-{
-    $route = $request->getAttribute('route');
-    $userId = $route->getArgument('userId');
-    $result = $bot->getProfile($userId);
-
-    return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-});
 $app->run();
